7df75dfd66d8101ed334cbe642b86c6be1a857e8
arbassett <arbassett4@outlook.com>
2023-03-28 21:11:19 -0400

    1 file modified

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,21 @@
-import { onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import { Title } from 'solid-start';
 import Counter from '~/components/Counter';
 import { client } from '~/lib/trpc';
 
 export default function Home() {
+  const [trpcRes, setTrpcRes] = createSignal('');
   onMount(async () => {
-    console.log(await client.hello.query('test'));
+    const res = await client.hello.query('test');
+    console.log(res);
+    setTrpcRes(res);
   });
 
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
+      <div>trpc response {trpcRes()}</div>
       <Counter />
       <p>
         Visit{' '}
